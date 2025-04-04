84f959003949ee0bf7ef859b4622836bceef45ae
Ansh Tiwatne <ansh.tiwatne@gmail.com>
2025-04-04 15:52:29 +0530

    initial commit

---
+++ src/post.ts
@@ -0,0 +1,69 @@
+import fs from "fs/promises"
+import path from "path"
+import puppeteer, { Page } from "puppeteer"
+
+const postsDir = path.join(__dirname, "../posts")
+
+interface PostData {
+	filePath: string
+	post: {
+		date: string
+		title: string
+		content: string
+	}
+}
+
+async function getPosts() {
+	const postFiles = await fs.readdir(postsDir)
+	const posts: PostData[] = []
+
+	for (const postFile of postFiles) {
+		const filePath = path.join(postsDir, postFile)
+		const post = JSON.parse(await fs.readFile(filePath, "utf-8"))
+		posts.push({ filePath, post })
+	}
+
+	posts.sort(
+		(a, b) =>
+			new Date(a.post.date).getTime() - new Date(b.post.date).getTime()
+	)
+
+	return posts
+}
+
+async function populatePost(page: Page, post) {
+	await page.locator('[aria-label="Add title"]').fill(post.title)
+
+	await page.focus(".block-editor-default-block-appender__content")
+	await page.evaluate((content) => {
+		return navigator.clipboard.writeText(content)
+	}, post.content)
+
+	await page.keyboard.down("Control")
+	await page.keyboard.press("V")
+	await page.keyboard.up("Control")
+
+	await new Promise((resolve) => setTimeout(resolve, 1000))
+}
+
+async function main() {
+	const browser = await puppeteer.launch({ headless: false })
+	const page = await browser.newPage()
+	const posts = await getPosts()
+
+	for (const { post } of posts) {
+		await page.goto("https://www.livingacademy.org/wp-admin/post-new.php")
+		await populatePost(page, post)
+
+		await page.locator(".editor-post-publish-button__button").click()
+		await page.locator(".editor-post-publish-panel").wait()
+		await page.locator(".editor-post-publish-button__button").click()
+
+		await page.locator(".components-snackbar__content").wait()
+		await new Promise((resolve) => setTimeout(resolve, 1000))
+	}
+
+	await browser.close()
+}
+
+main()
---
+++ src/scrape.ts
@@ -0,0 +1,81 @@
+import fs from "fs"
+import path from "path"
+import puppeteer from "puppeteer"
+
+const postsDir = path.join(__dirname, "../posts")
+if (!fs.existsSync(postsDir)) {
+	fs.mkdirSync(postsDir, { recursive: true })
+}
+
+function sanitizeFilename(name: string): string {
+	return name.replace(/[\/\\?%*:|"<>]/g, "-")
+}
+
+function sanitizeDate(date: string): Date {
+	return new Date(date)
+}
+
+function unwrapImgAnchors(html: string): string {
+	return html.replace(/<a\b[^>]*>\s*(<img\b[^>]*>).*?<\/a>/gi, "$1")
+}
+
+function savePost(
+	title: string,
+	author: string,
+	date: string,
+	postHTML: string
+) {
+	console.log(title)
+
+	const postData = {
+		title: title,
+		author: author,
+		date: sanitizeDate(date),
+		content: unwrapImgAnchors(postHTML),
+	}
+
+	const jsonContent = JSON.stringify(postData, null, "\t")
+	const fileName = path.join(postsDir, `${sanitizeFilename(title)}.json`)
+
+	fs.writeFileSync(fileName, jsonContent, "utf8")
+}
+
+async function main() {
+	const browser = await puppeteer.launch()
+	const page = await browser.newPage()
+
+	await page.goto("https://metastudio.org/c/la/88")
+	await page.locator(".topic-list-body").wait()
+
+	const postElements = await page.$$("a.raw-topic-link")
+	for (const element of postElements) {
+		const postLink = await page.evaluate((el) => el.href, element)
+
+		const postPage = await browser.newPage()
+		await postPage.goto(postLink)
+
+		const postHTML = await postPage.$eval(
+			"div.cooked",
+			(el) => el.innerHTML
+		)
+		const title =
+			(await postPage.$eval("a.fancy-title", (el) =>
+				el.textContent?.trim()
+			)) || ""
+		const author =
+			(await postPage.$eval("span.full-name a", (el) =>
+				el.textContent?.trim()
+			)) || ""
+		const date =
+			(await postPage.$eval("span.relative-date", (el) =>
+				el.getAttribute("title")
+			)) || ""
+		await postPage.close()
+
+		savePost(title, author, date, postHTML)
+	}
+
+	await browser.close()
+}
+
+main()
